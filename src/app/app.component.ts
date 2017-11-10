e0075767a525fb4586d4bce849ad2ad611c44759
桑智勇 <sangzhiyong@linkcircle.cn>
2017-11-10 14:15:07 +0800

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,83 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  			<h2>{{hero.name}} details!</h2>
-  			<div><label>id: </label>{{hero.id}}</div>
-  			<div><label>name: </label>{{hero.name}}</div>
-  			<div>
-  				<label>name: </label>
-  				<input [(ngModel)]="hero.name" placeholder="name"/>
+  template: `
+  			<h2>My Heroes</h2>
+  			<ul class="heroes">
+  				<li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+  					<!--each hero goes here -->
+  					<span class="badge">{{hero.id}}</span>{{hero.name}}
+  				</li>
+  			</ul>
+  			<div *ngIf="selectedHero">
+	  			<h2>{{selectedHero.name}} details!</h2>
+	  			<div><label>id: </label>{{selectedHero.id}}</div>
+	  			<div>
+	  				<label>name: </label>
+	  				<input [(ngModel)]="selectedHero.name" placeholder="name"/>
+	  			<div>
   			</div>
-  			`
+  			`,
+  styles: [`
+  	.selected {
+  		background-color: #CFD8DC !important;
+  		color: white;
+  	}
+  	.heroes {
+  		margin: 0 0 2em 0;
+  		list-style-type: none;
+  		padding: 0;
+  		width : 15em;
+  	}
+  	.heroes li {
+  		cursor: pointer;
+  		position: relative;
+  		left: 0;
+  		background-color: #eee;
+  		margin: 0.5em;
+  		padding: .3em 0;
+  		height: 1.6em;
+  		border-radius: 4px;
+  	}
+  	.heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+  `],
 })
-//[[ngModel]] 是一个angular语法，用与把hero.name 绑定到输入框中
-//他的数据流是双向的：从属性到输入框，并从输入框回到属性
+//ngFor 的*前缀表示<li>及其子元素组成了一个主控模板
+//ngFor 指令在AppComponent.heroes属性返回的heroes数组上迭代，并输出此模板的实例
 export class AppComponent  { 
 	title = 'Tour of Heros';
-	hero : Hero ={
-		id: 1,
-		name: 'Windstorm'
-	};
+	heroes = HEROES;
+	selectedHero: Hero;
+
+	onSelect(hero:Hero): void {
+		this.selectedHero = hero;
+	}
 }
 export class Hero {
 	id : number;
